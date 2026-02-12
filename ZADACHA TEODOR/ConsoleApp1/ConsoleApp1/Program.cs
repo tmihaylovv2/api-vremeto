66117059bc7b177d3924952baadcc3cf726bab34
tmihaylovv2 <teodormihaylov63@gmail.com>
2026-02-12 12:09:03 +0200

    Add files via upload

---
+++ ZADACHA TEODOR/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        var json = new WebClient().DownloadString(
+            "https://api.open-meteo.com/v1/forecast?latitude=42.1354&longitude=24.7453&current_weather=true");
+
+        var w = JObject.Parse(json)["current_weather"];
+
+        Console.WriteLine("Vremeto v Plovdiv");
+        Console.WriteLine("------------------");
+        Console.WriteLine($"Temperature: {w["temperature"]}°C");
+        Console.WriteLine($"Wind: {w["windspeed"]} km/h");
+    }
+}
---
+++ ZADACHA TEODOR/task/task/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) 
+            {
+
+ 
+        }
+    }
+}
